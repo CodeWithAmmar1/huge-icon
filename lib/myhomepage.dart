efadc1a41ade06e088d0964225502215b2f2ad3b
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-06 18:11:47 +0500

    icons testing

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -17,66 +17,69 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedAdd02,
-              color: Colors.green,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedAccountSetting01,
-              color: Colors.red,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedZoomCircle,
-              color: Colors.orange,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedZakat,
-              color: Colors.pink,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedYogaBall,
-              color: Colors.yellow,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedAddCircle,
-              color: Colors.blue,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedTwitter,
-              color: Colors.blue,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedTiktok,
-              color: Colors.black,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedInstagram,
-              color: Colors.brown,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedWhatsapp,
-              color: Colors.lightGreenAccent,
-              size: 40,
-            ),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedGoogle,
-              color: Colors.blue,
-              size: 40,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedAdd02,
+                color: Colors.green,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedAccountSetting01,
+                color: Colors.red,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedZoomCircle,
+                color: Colors.orange,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedZakat,
+                color: Colors.pink,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedYogaBall,
+                color: Colors.yellow,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedAddCircle,
+                color: Colors.blue,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedTwitter,
+                color: Colors.blue,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedTiktok,
+                color: Colors.black,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedInstagram,
+                color: Colors.brown,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedWhatsapp,
+                color: Colors.lightGreenAccent,
+                size: 40,
+              ),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedGoogle,
+                color: Colors.blue,
+                size: 40,
+              ),
+            ],
+          ),
         ),
       ),
     );
